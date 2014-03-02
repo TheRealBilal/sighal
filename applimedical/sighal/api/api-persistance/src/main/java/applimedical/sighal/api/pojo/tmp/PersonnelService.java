d42e462c73b2f5b9da6d13d8ec36ce8740e6d504
Marouane.Hazem <marouane.hazem@gmail.com>
2014-03-02 17:38:50 +0000

    Nouvelles entités Service et PersonnelService.

---
+++ applimedical/sighal/api/api-persistance/src/main/java/applimedical/sighal/api/pojo/tmp/PersonnelService.java
@@ -0,0 +1,16 @@
+package applimedical.sighal.api.pojo.tmp;
+
+/***********************************************************************
+ * Module:  PersonnelService.java
+ * Author:  HAZEM
+ * Purpose: Defines the Class PersonnelService
+ ***********************************************************************/
+
+import java.util.*;
+
+/** @pdOid d7beae5a-28d0-4ac2-a22e-4dd4f3f20e39 */
+public class PersonnelService {
+   /** @pdOid 088601ac-2ee8-4572-b177-87eae298eba7 */
+   private boolean isServiceChef;
+
+}
---
+++ applimedical/sighal/api/api-persistance/src/main/java/applimedical/sighal/api/pojo/tmp/Service.java
@@ -0,0 +1,20 @@
+package applimedical.sighal.api.pojo.tmp;
+
+/***********************************************************************
+ * Module:  Service.java
+ * Author:  HAZEM
+ * Purpose: Defines the Class Service
+ ***********************************************************************/
+
+import java.util.*;
+
+/** @pdOid 2aad72b8-dc92-44b7-a525-370167bbecc5 */
+public class Service {
+   /** @pdOid fddfc81e-7dd1-41a9-8129-bea5e1d6bdfd */
+   private int serviceId;
+   /** @pdOid 98f234e4-dceb-4a36-907d-dc2f3272e482 */
+   private String nom;
+   /** @pdOid 43ed7b57-6b78-4f66-a233-54f5e5c69ed5 */
+   private String description;
+
+}

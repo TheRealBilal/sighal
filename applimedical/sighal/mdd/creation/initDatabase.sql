a9fbf92d68f74e66091bd17e08411817b51e1d7d
adim.bilal <adim.bilal@gmail.com>
2014-05-20 18:58:02 +0000

    data base

--- applimedical/sighal/mdd/creation/initDatabase.sql
+++ applimedical/sighal/mdd/creation/initDatabase.sql
@@ -2,7 +2,7 @@
 mysql -h localhost -u root;
 
 /--Creation d'un utilisateur--/
-GRANT ALL PRIVILEGES ON *.* TO 'sighalWebUser'@'localhost' IDENTIFIED BY '123456' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON sighal_database.sighal_database TO 'sighalWebUser'@'localhost' IDENTIFIED BY '123456' WITH GRANT OPTION;
 
 /--Creation d'une base de donnée--/
 CREATE DATABASE sighal_database;

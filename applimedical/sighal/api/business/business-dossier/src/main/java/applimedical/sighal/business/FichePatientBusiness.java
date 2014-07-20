0f92e6fece89104353ba752986b7dac60da891d1
lamine.aissioui <lamine.aissioui@99f43f00-1506-6762-806a-c30b31286b87>
2014-07-20 19:40:03 +0000

    add FichePatient and FichePatientImpl to business-dossiers project + update pom.xml

---
+++ applimedical/sighal/api/business/business-dossier/src/main/java/applimedical/sighal/business/FichePatientBusiness.java
@@ -0,0 +1,13 @@
+package applimedical.sighal.business;
+
+import java.util.List;
+
+import applimedical.sighal.dto.FichePatientDto;
+
+public interface FichePatientBusiness {
+	List<FichePatientDto> getToutesLesFichesPatient();
+	FichePatientDto getFichePatienParId(Long fichePatientId);
+	//List<FichePatientDto> getFichesPatient(String code, String nom);
+	FichePatientDto sauvegarderFichePatient(FichePatientDto fichePatientDto);
+	void supprimerSalle(Long fichePatientId);
+}
---
+++ applimedical/sighal/api/business/business-dossier/src/main/java/applimedical/sighal/business/impl/FichePatientBusinessImpl.java
@@ -0,0 +1,100 @@
+package applimedical.sighal.business.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import applimedical.sighal.api.pojo.FichePatient;
+import applimedical.sighal.api.pojo.Personnel;
+import applimedical.sighal.api.pojo.Salle;
+import applimedical.sighal.business.FichePatientBusiness;
+import applimedical.sighal.dto.FichePatientDto;
+import applimedical.sighal.dto.SalleDto;
+
+import applimedical.sighal.dao.FichePatientRepository;
+import applimedical.sighal.dao.PersonnelRepository;
+
+@Service("FichePatientBusiness")
+@Transactional
+public class FichePatientBusinessImpl implements FichePatientBusiness {
+	@Autowired
+	private FichePatientRepository fichePatientRepo;
+	private PersonnelRepository personnelRepository;
+	public List<FichePatientDto> getToutesLesFichesPatient() {
+		List<FichePatient> fichePatients= fichePatientRepo.findAll();
+		List<FichePatientDto> fichePatientDtos = new ArrayList<FichePatientDto>( );
+		for(FichePatient fichePatient : fichePatients){
+			FichePatientDto fichePatientDto = new FichePatientDto();
+			fichePatientDto.setDateCreation(fichePatient.getDateCreation());
+			fichePatientDto.setDateModification(fichePatient.getDateModification());
+			//fichePatientDto.setDossierPatientDto(getDossierPatientParId(fichePatient.getDossierPatient().getDossierPatientId()));
+			fichePatientDto.setEtatGeneraleActuelPatient(fichePatient.getEtatGeneraleActuelPatient());
+			fichePatientDto.setFichePatientId(fichePatient.getFichePatientId());
+			//fichePatientDto.setInterventionDtoList()
+			fichePatientDto.setObservation(fichePatient.getObservation());
+			fichePatientDto.setPersonnelDto(new PersonnelBusinessImpl().getPersonnelDtoParId(fichePatient.getPersonnel().getPersonneId()));;
+			fichePatientDto.setPlanTraitement(fichePatient.getPlanTraitement());
+			fichePatientDto.setTypeFichePatient(fichePatient.getTypeFichePatient());
+			fichePatientDtos.add(fichePatientDto);	
+			
+		}
+		
+		return fichePatientDtos;
+	}
+
+	public FichePatientDto getFichePatienParId(Long fichePatientId) {
+		
+	
+		FichePatient fichePatient = fichePatientRepo.findOne(fichePatientId);
+
+			FichePatientDto fichePatientDto = new FichePatientDto();
+			fichePatientDto.setDateCreation(fichePatient.getDateCreation());
+			fichePatientDto.setDateModification(fichePatient.getDateModification());
+			//fichePatientDto.setDossierPatientDto();
+			fichePatientDto.setEtatGeneraleActuelPatient(fichePatient.getEtatGeneraleActuelPatient());
+			fichePatientDto.setFichePatientId(fichePatient.getFichePatientId());
+			//fichePatientDto.setInterventionDtoList()
+			fichePatientDto.setObservation(fichePatient.getObservation());
+			fichePatientDto.setPersonnelDto(new PersonnelBusinessImpl().getPersonnelDtoParId(fichePatientId));
+			fichePatientDto.setPlanTraitement(fichePatient.getPlanTraitement());
+			fichePatientDto.setTypeFichePatient(fichePatient.getTypeFichePatient());	
+			
+		return fichePatientDto;
+	}
+
+	public FichePatientDto sauvegarderFichePatient(FichePatientDto fichePatientDto){
+			
+			FichePatient fichePatient;
+			      if (fichePatientDto.getFichePatientId() == null) {
+			         fichePatient = new FichePatient();
+			      } else {
+			         fichePatient = fichePatientRepo.findOne(fichePatientDto.getFichePatientId());
+			      }
+			      fichePatient.setDateCreation(fichePatientDto.getDateCreation());
+			      fichePatient.setDateModification(fichePatientDto.getDateModification());
+			      //fichePatient.setDossierPatient();
+			      fichePatient.setEtatGeneraleActuelPatient(fichePatientDto.getEtatGeneraleActuelPatient());
+			      //fichePatient.setFichePatientId(fichePatientDto.getFichePatientId());fiche patient dto possède un id en creation?
+			      //fichePatient.setInterventionList();
+			      fichePatient.setObservation(fichePatientDto.getObservation());	
+			      
+			      fichePatient.setPersonnel(personnelRepository.findOne(fichePatientDto.getPersonnelDto().getPersonneId()));
+			      fichePatient.setPlanTraitement(fichePatientDto.getPlanTraitement());
+			      fichePatient.setTypeFichePatient(fichePatientDto.getTypeFichePatient());
+			      
+			      
+			      FichePatient newFichePatient = fichePatientRepo.save(fichePatient);
+			      fichePatientDto.setFichePatientId(newFichePatient.getFichePatientId());
+			      return fichePatientDto;
+			   
+	}
+
+	public void supprimerSalle(Long fichePatientId) {
+		fichePatientRepo.delete(fichePatientId);
+
+	}
+
+}

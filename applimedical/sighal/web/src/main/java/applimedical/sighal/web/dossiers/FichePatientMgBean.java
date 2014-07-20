ddb48a049732006ea3c9d65c5464c1bb5f87b419
lamine.aissioui <lamine.aissioui@99f43f00-1506-6762-806a-c30b31286b87>
2014-07-20 19:41:36 +0000

    add fiche patient presentation couche (just consult screen)

---
+++ applimedical/sighal/web/src/main/java/applimedical/sighal/web/dossiers/FichePatientMgBean.java
@@ -0,0 +1,80 @@
+package applimedical.sighal.web.dossiers;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import applimedical.sighal.business.FichePatientBusiness;
+import applimedical.sighal.dto.FichePatientDto;
+import org.primefaces.event.FlowEvent;
+
+@Controller("FichePatientBn")
+@ManagedBean
+@ViewScoped
+public class FichePatientMgBean {
+	@Autowired
+	private FichePatientBusiness fichePatientBusiness;
+	private FichePatientDto selectedFichePatient ;
+	private List<FichePatientDto> fichepatientList;
+	
+	 @PostConstruct
+	   public void init() {
+		 selectedFichePatient = new FichePatientDto();
+		 fichepatientList = fichePatientBusiness.getToutesLesFichesPatient();
+	   }
+	 
+	 public String startPage() {
+	      selectedFichePatient = new FichePatientDto();
+	      fichepatientList = fichePatientBusiness.getToutesLesFichesPatient();
+	      return "listeFichePatient";
+	   }
+	 public String rechercher() {
+	      return "listeFichePatient";
+	   }
+
+	   public String initCreer() {
+	      selectedFichePatient = new FichePatientDto();
+	      return "editionFichePatient";
+	   }
+
+	   public String initModifier(FichePatientDto fichePatientDto) {
+	      selectedFichePatient = fichePatientDto;
+	      return "editionFichePatient";
+	   }
+
+	   public String sauvegarder() {
+		   fichePatientBusiness.sauvegarderFichePatient(selectedFichePatient);
+	      return startPage();
+	   }
+
+	   public String supprimer(String fichepatientId) {
+	      fichePatientBusiness.supprimerSalle(Long.valueOf(fichepatientId));
+	      return startPage();
+	   }
+	   public String onFlowProcess(FlowEvent event) {
+	        
+	            return event.getNewStep();
+	        }
+
+	   public FichePatientDto getselectedFichePatient() {
+	      return selectedFichePatient;
+	   }
+
+	   public void setselectedFichePatient(FichePatientDto selectedFichePatient) {
+	      this.selectedFichePatient = selectedFichePatient;
+	   }
+
+	   public List<FichePatientDto> getfichepatientList() {
+	      return fichepatientList;
+	   }
+
+	   public void setfichepatientList(List<FichePatientDto> fichepatientList) {
+	      this.fichepatientList = fichepatientList;
+	   }
+	 
+}
